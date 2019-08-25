8342db7320f6402bee8e725b3baa0a2c672b5a9b
Codey Whitt <codey.whitt@gmail.com>
2019-08-24 22:24:00 -0600

    add scaffolding for coinbase

---
+++ types/provider/coinbasepro/coinbasepro.go
@@ -0,0 +1,63 @@
+package coinbasepro
+
+import (
+	"time"
+
+	"github.com/sinisterminister/currencytrader/types"
+)
+
+type provider struct {
+	trader types.Trader
+}
+
+func New(trader types.Trader) types.Provider {
+	return &provider{trader}
+}
+
+func (p *provider) AttemptOrder(req types.OrderRequestDTO) (dto types.OrderDTO, err error) {
+	return
+}
+
+func (p *provider) CancelOrder(ord types.OrderDTO) (err error) {
+	return
+}
+
+func (p *provider) Candles(mkt types.MarketDTO, interval types.CandleInterval, start time.Time, end time.Time) (candles []types.CandleDTO, err error) {
+	return
+}
+
+func (p *provider) Currencies() (curs []types.CurrencyDTO, err error) {
+	return
+}
+
+func (p *provider) Markets() (mkts []types.MarketDTO, err error) {
+	return
+}
+
+func (p *provider) Order(markets types.MarketDTO, id string) (ord types.OrderDTO, err error) {
+	return
+}
+
+func (p *provider) OrderStream(stop <-chan bool, order types.OrderDTO) (stream <-chan types.OrderDTO, err error) {
+	return
+}
+
+func (p *provider) Ticker(market types.MarketDTO) (tkr types.TickerDTO, err error) {
+	return
+}
+
+func (p *provider) TickerStream(stop <-chan bool, market types.MarketDTO) (stream <-chan types.TickerDTO, err error) {
+	return
+}
+
+func (p *provider) Wallet(currency types.CurrencyDTO) (wal types.WalletDTO, err error) {
+	return
+}
+
+func (p *provider) Wallets() (wals []types.WalletDTO, err error) {
+	return
+}
+
+func (p *provider) WalletStream(stop <-chan bool, currency types.CurrencyDTO) (stream <-chan types.WalletDTO, err error) {
+	return
+}
